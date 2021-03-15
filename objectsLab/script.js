dbec3e02cf81a8f9d8be6125a0c19a353b07eac0
Jinjo-bit <71643734+Jinjo-bit@users.noreply.github.com>
2021-03-15 10:10:33 -0400

    Add files via upload

---
+++ objectsLab/script.js
@@ -0,0 +1,8 @@
+let dog = {
+    name: "buddy",
+    numLegs: 4,
+    sayLegs: function() {return "Le Doge has 4 legs...." +  this.name + ".";}
+    
+};
+dog.sayLegs();
+document.getElementById("doggy");
